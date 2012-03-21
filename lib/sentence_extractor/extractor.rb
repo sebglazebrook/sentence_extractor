827ce8cbb1d84cb50dffe122fa73569d89020347
Sebastian Glazebrook <me@sebglazebrook.com>
2012-03-21 15:32:38 +1100

    added all methods for a basic sentence_extractor. It's now ready for testing

--- lib/sentence_extractor/extractor.rb
+++ lib/sentence_extractor/extractor.rb
@@ -7,6 +7,7 @@ module SentenceExtractor
     @text = nil
     @delimiters = nil
     @first_letter_capitilization = nil
+    @reg_exp = nil
     
     def initialize( language = "en", delimiters = [".", "!","?"], first_letter_capitilization = false)
       @language = language
@@ -23,11 +24,34 @@ module SentenceExtractor
     # returns an array of sentences
     def get_sentences
       sentences = Array.new
-      # go through array of delimiters and break up the strings.
-      @delimiters.each do |delimiter|
-        #@TODO
+      if possible_sentences?
+        sentences = get_array_of_sentences
       end
+      sentences
+    end
+    
+    def possible_sentences?
+      # build regexp
+      @reg_exp = "\\" + @delimiters.join("|\\")
+      # check for any delimiters       
+      @text =~ /#{@reg_exp}/
+    end
+    
+    def get_array_of_sentences
+      sentences = Array.new
+      remaining_text = @text
+      match = true
+      # go through and find all sentences
+      while match != nil
+        sentence_end_point = remaining_text =~ /#{@reg_exp}/  # check for sentences
+        if sentence_end_point
+          sentences << remaining_text[0..sentence_end_point]  # add new sentence to array
+          remaining_text = remaining_text[sentence_end_point+1..remaining_text.size]  # set the rest of the text to be processed.
+        end
+      end
+      sentences
     end
     
    end
+   
 end
